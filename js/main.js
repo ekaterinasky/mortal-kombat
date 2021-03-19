86c367cc8fcb74560c69c10985ab1701205e7529
ekaterinasky <74653982+ekaterinasky@users.noreply.github.com>
2021-03-19 10:39:29 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+$('#app').vide({
+  mp4: 'video/video.mp4',
+  webm: 'video/video.webm',
+  ogv: 'video/video.ogv',
+  //poster: 'img/cover-image.jpg'
+},
+);
+
+$('#myBlock').vide('extended path as a string', 'options as a string');
+})
